a5073067174e0da1e2e3e05ab6ad3a2b4b7093e5
Utkarsh Tiwari <tiwariutkarsh007@gamil.com>
2019-01-28 16:28:18 +0530

    Create product model and configure database

---
+++ application/controllers/Products.php
@@ -0,0 +1,12 @@
+<?php
+
+class Products extends CI_Controller{
+    public function index(){
+        $data['title'] = 'Latest Products';
+        $data['products'] = $this->product_model->get_products();
+
+        $this->load->view('templates/header');
+        $this->load->view('products/index', $data);
+        $this->load->view('templates/footer');
+    }
+}
---
+++ application/models/Product_model.php
@@ -0,0 +1,16 @@
+<?php
+
+class Product_model extends CI_Model{
+    public function __construct(){
+        $this->load->database();
+    }
+
+    public function get_products($slug = FALSE){
+        if($slug === FALSE){
+            $query = $this->db->get('products');
+            return $query->result_array();
+        }
+        $query = $this->db->get('products', array('slug' => $slug));
+        return $query->row_array();
+    }
+}
