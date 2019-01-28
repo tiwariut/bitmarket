d1dae03adf08b9041c07a16828ba0fa2740c96dd
Utkarsh Tiwari <tiwariutkarsh007@gamil.com>
2019-01-28 19:15:45 +0530

    Style index and view pages

--- application/views/products/index.php
+++ application/views/products/index.php
@@ -1,12 +1,20 @@
 <h2><?= $title ?></h2>
 
-<?php foreach($products as $product) : ?>
-   <h3><?php echo $product['title']; ?></h3>
-   <img src="<?php echo $product['image']; ?>"></img>
-    <h4><?php echo $product['price']; ?></h4>
-    <p><a class="btn btn-default" href="<?php echo site_url('/products/' . $product['slug']); ?>">View</a></p>
+<div class="container">
 
-<?php endforeach; ?>
-
-
+    <div class="row">
+        <?php foreach($products as $product) : ?>
+        <div class="col-sm">
+            <div class="card" style="width: 18rem;">
+                <img src="<?php echo $product['image']; ?>" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $product['title']; ?></h5>
+                    <p class="card-text"><strong>Price:</strong> Rs. <?php echo $product['price']; ?></p>
+                    <a href="<?php echo site_url('/products/' . $product['slug']); ?>" class="btn btn-primary">View</a>
+                </div>
+            </div>
+        </div>
+        <?php endforeach; ?>
+    </div>
+</div>
 
--- application/views/products/view.php
+++ application/views/products/view.php
@@ -1,4 +1,13 @@
-<h2><?php echo $product['title']; ?></h2>
-<img src="<?php echo $product['image']; ?>">
-<h5><?php echo $product['price']; ?></h5>
-<p><?php echo $product['body']; ?></p>
+
+<div class="container">
+    <div class="card border-secondary bg-light mb-3">
+        <h1 class="heading card-title" style="margin-top: 45px;"><?php echo $product['title']; ?></h1>
+        <div class="card-body">
+            <hr>
+            <img class="card-img-top" style="max-width: 300px;" src="<?php echo $product['image']; ?>" alt="Card image cap">
+            <hr>
+            <p class="card-text"><strong>Price:</strong> Rs. <?php echo $product['price']; ?></p>
+            <p class="card-text"><?php echo $product['body']; ?></p>
+        </div>
+    </div>
+</div>
