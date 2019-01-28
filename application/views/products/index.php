f15002ede4c5c8c755cf316531971c9854adad8a
Utkarsh Tiwari <tiwariutkarsh007@gamil.com>
2019-01-28 17:04:10 +0530

    View products on index page

--- application/views/products/index.php
+++ application/views/products/index.php
@@ -1,1 +1,6 @@
 <h2><?= $title ?></h2>
+
+<?php foreach($products as $product) : ?>
+   <h3><?php echo $product['title']; ?></h3>
+   <p></p>
+<?php endforeach; ?>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,11 +2,12 @@
    <head>
        <title>bitMarket</title>
        <link rel="stylesheet" type="text/css" href="https://bootswatch.com/4/lumen/bootstrap.min.css">
+       <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/css/style.css">
    </head>
    <body>
 
    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-       <a class="navbar-brand" href="#">bitMarket</a>
+       <a class="navbar-brand" href="<?php echo base_url(); ?>">bitMarket</a>
        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
            <span class="navbar-toggler-icon"></span>
        </button>
@@ -18,6 +19,9 @@
                </li>
                <li class="nav-item">
                    <a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
+               </li>
+               <li class="nav-item">
+                   <a class="nav-link" href="<?php echo base_url(); ?>products">Products</a>
                </li>
            </ul>
        </div>
